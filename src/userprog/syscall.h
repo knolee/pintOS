5d11acb11cf29620de75e489c5f3ca08e8560989
aklum3 <49923615+aklum3@users.noreply.github.com>
2020-10-05 01:59:34 -0700

    Update syscall.h

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -9,28 +9,28 @@ int check_pointer(void *pointer);
 // System Call: halt N
 void halt (void);
 // System Call: exit N
-void exit (int status);
+//void exit (int status);
 // System Call: exec N
-pid_t exec (const char *cmd_line);
+//pid_t exec (const char *cmd_line);
 // System Call: wait N
-int wait (pid_t pid);
+//int wait (pid_t pid);
 // System Call: create N
-bool create (const char *file, unsigned initial_size);
+//bool create (const char *file, unsigned initial_size);
 // System Call: remove N
-bool remove (const char *file);
+//bool remove (const char *file);
 // System Call: open N
-int open (const char *file);
+//int open (const char *file);
 // System Call: filesize N
-int filesize (int fd);
+//int filesize (int fd);
 // System Call: read N
-int read (int fd, void *buffer, unsigned size);
+//int read (int fd, void *buffer, unsigned size);
 // System Call: write N
-int write (int fd, const void *buffer, unsigned size);
+//int write (int fd, const void *buffer, unsigned size);
 // System Call: seek N
-void seek (int fd, unsigned position);
+//void seek (int fd, unsigned position);
 // System Call: tell N
-unsigned tell (int fd);
+//unsigned tell (int fd);
 // System Call: close N
-void close (int fd);
+//void close (int fd);
 
 #endif /* userprog/syscall.h */
