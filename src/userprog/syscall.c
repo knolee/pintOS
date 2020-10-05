5f27b7c833c05a5be26542b6d0ca60c597ac8ef9
aklum3 <49923615+aklum3@users.noreply.github.com>
2020-10-05 02:21:06 -0700

    added break to switch statement

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -27,7 +27,7 @@ syscall_handler (struct intr_frame *f UNUSED)
     case SYS_HALT:
       printf("syscall: halt\n");
       halt();
-
+      break;
     case SYS_EXIT:
       printf("syscall: exit\n");
 
